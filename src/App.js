b3c599784696df5d7ef108ff5c03186096756a43
hostelbetting <hostelbetting.fun@gmail.com>
2025-02-17 01:32:43 +0530

    Refactor App component to use custom hook for current user and update profile completion prompt visibility based on authentication state

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { useContext, useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { useLocation, useNavigate } from 'react-router-dom';
 import AuthContext from './contexts/AuthContext';
+import { useCurrentUser } from './hooks/current-user';
 
 function App() {
   const [showNav, setShowNav] = useState(true);
@@ -21,13 +22,14 @@ function App() {
   }, [location])
 
   const navigate = useNavigate()
-  const { currentUser } = useContext(AuthContext);
+  const { isAuthenticated } = useContext(AuthContext);
+  const currentUser = useCurrentUser();
 
   return (
     <>
       {showNav && <Navbar />}
       <main>
-        {showNav && (!currentUser?.isBloodGropAdded && !currentUser?.isContactInfoFilled) &&
+        {showNav && (isAuthenticated && !currentUser?.isBloodGropAdded && !currentUser?.isContactInfoFilled) &&
           <div style={{ width: "100%" }} className='d-flex justify-content-center'>< div className='ph-nav-flag' onClick={() => navigate("/profile-edit")}>
             <div>Complete your profile to access the all features. Add address and blood group.</div>
             <div><span><i class="ri-arrow-right-s-line"></i></span></div>
